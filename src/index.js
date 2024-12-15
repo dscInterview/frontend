cdcb712c97d27d00e052aefee4e6d92607b99b57
Hemanth S <gowda.hemanth.1718@gmail.com>
2024-12-16 01:55:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";  // Import BrowserRouter
+import { Provider } from "react-redux";  // Import the Provider component from react-redux
+import store from "./redux/store";  // Import your store
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>  {/* Wrap App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>  {/* Wrap App with Provider */}
+      <BrowserRouter>  {/* Wrap App with BrowserRouter */}
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
