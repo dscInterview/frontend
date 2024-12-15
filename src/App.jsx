1544728c4e918307eea85a6452ab49393d95f6e0
Hemanth S <gowda.hemanth.1718@gmail.com>
2024-12-16 01:55:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.js
-import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes,Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
@@ -9,14 +9,20 @@ import AboutPage from "./Pages/AboutPage";
 import ContactPage from "./Pages/ContactPage";
 import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer"; // Import Footer
-
+import { initializeAuth } from './redux/slices/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 const App = () => {
-  const [isLogged, setIsLogged] = useState(false);
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(initializeAuth());
+  }, [dispatch]);
 
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <Navbar isLogged={isLogged} setIsLogged={setIsLogged} />
+      <Navbar/>
       
       {/* Main Content Area */}
       <main className="flex-grow">
@@ -25,13 +31,15 @@ const App = () => {
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/sign-up" element={<SignupForm />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<LoginForm setIsLogged={setIsLogged} />} />
+          <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />}
+        />
+          <Route path="/login" element={<LoginForm />} />
         </Routes>
       </main>
       
       {/* Footer */}
       <Footer />
+      <ToastContainer />
     </div>
   );
 };
